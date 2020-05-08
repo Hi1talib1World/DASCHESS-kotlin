1616462a4650261a4e85969b4ad5703904af026f
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-08 13:18:43 +0000

    Update Board.kt

--- app/src/main/java/com/denzo/daschess/model/board/Board.kt
+++ app/src/main/java/com/denzo/daschess/model/board/Board.kt
@@ -1,2 +1,14 @@
 package com.denzo.daschess.model.board
 
+import com.alltimeslucky.cheekychess.model.piece.Piece
+
+interface Board {
+
+    fun getGridElement(gridCoordinates: Pair<Int, Int>): Piece?
+    fun getHeight(): Int
+    fun getWidth(): Int
+    fun initializeGrid()
+    fun isOccupied(gridCoordinates: Pair<Int, Int>): Boolean
+    fun move(firstGridCoordinates: Pair<Int, Int>, secondGridCoordinates: Pair<Int, Int>): Boolean
+
+}
