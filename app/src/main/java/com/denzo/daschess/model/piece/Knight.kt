33d253cfada1be9fdd9b1187fdd69e82794f743c
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-08 13:27:15 +0000

    Create Knight.kt

---
+++ app/src/main/java/com/denzo/daschess/model/piece/Knight.kt
@@ -0,0 +1,14 @@
+package com.denzo.daschess.model.piece
+
+import com.denzo.daschess.model.board.Board
+
+
+class Knight(override val colour: Colour) : Piece() {
+
+    override val pointValue = 3
+
+    override fun getPossibleMoveSquares(board: Board, gridCoordinates: Pair<Int, Int>): List<Pair<Int, Int>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
