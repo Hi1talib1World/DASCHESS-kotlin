83c4ced19adc2196779c8150795ece7b8fb562be
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-08 12:42:41 +0000

    Update MainActivity.kt

--- app/src/main/java/com/denzo/daschess/MainActivity.kt
+++ app/src/main/java/com/denzo/daschess/MainActivity.kt
@@ -1,6 +1,21 @@
 package com.denzo.daschess
 
 import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.v7.app.AppCompatActivity
+import android.view.MotionEvent
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
+import com.alltimeslucky.cheekychess.controller.SelectionController
+import com.alltimeslucky.cheekychess.koin.Module
+import com.alltimeslucky.cheekychess.model.board.Board
+import com.alltimeslucky.cheekychess.view.CoordinateMapper
+import com.alltimeslucky.cheekychess.view.board.BoardRenderer
+import org.koin.android.ext.android.inject
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
