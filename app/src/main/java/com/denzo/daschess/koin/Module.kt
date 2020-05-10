987dca51632ad0815eaa39b0c8b0c02f9ddc30ac
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-10 12:42:31 +0000

    Update Module.kt

--- app/src/main/java/com/denzo/daschess/koin/Module.kt
+++ app/src/main/java/com/denzo/daschess/koin/Module.kt
@@ -1,7 +1,18 @@
 package com.denzo.daschess.koin
 
+import com.denzo.daschess.controller.SelectionController
 import com.denzo.daschess.model.board.Board
 import com.denzo.daschess.model.board.BoardImpl
+import com.denzo.daschess.model.piece.PieceFactory
+import com.denzo.daschess.model.piece.PieceFactoryImpl
+import com.denzo.daschess.model.selection.Selection
+import com.denzo.daschess.model.selection.SelectionImpl
+import com.denzo.daschess.view.CoordinateMapper
+import com.denzo.daschess.view.board.BoardRenderer
+import com.denzo.daschess.view.board.BoardRendererImpl
+import com.denzo.daschess.view.piece.PieceRenderer
+import com.denzo.daschess.view.piece.PieceRendererImpl
+import org.koin.dsl.module
 
 
 class Module {
