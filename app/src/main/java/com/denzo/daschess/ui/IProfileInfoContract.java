75f49e4788e96aa765b4c21db83aa49141a91760
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-13 20:40:23 +0100

    Create IProfileInfoContract.java

---
+++ app/src/main/java/com/denzo/daschess/ui/IProfileInfoContract.java
@@ -0,0 +1,16 @@
+package com.denzo.daschess.ui;
+
+import java.util.ArrayList;
+
+public interface IProfileInfoContract {
+
+    interface View extends IBaseContract.View, IBasePagerContract.View{
+        void showProfileInfo(User user);
+        void showUserOrgs(ArrayList<User> orgs);
+    }
+
+    interface Presenter extends IBasePagerContract.Presenter<IProfileInfoContract.View>{
+
+    }
+
+}
