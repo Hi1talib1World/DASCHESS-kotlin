118c70c666fdb49ae27a6826b0c3fec9761e69c1
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-14 07:12:48 +0100

    Create SplashPresenter.java

---
+++ app/src/main/java/com/denzo/daschess/presenter/SplashPresenter.java
@@ -0,0 +1,113 @@
+package com.denzo.daschess.presenter;
+
+
+import android.database.Observable;
+
+import androidx.annotation.NonNull;
+
+import com.denzo.daschess.ui.User;
+
+import java.util.Date;
+
+import javax.inject.Inject;
+
+import retrofit2.Response;
+
+public class SplashPresenter extends BasePresenter<ISplashContract.View>
+        implements ISplashContract.Presenter{
+
+    private final String TAG = "SplashPresenter";
+
+    private AuthUser authUser;
+    private boolean isMainPageShowwed = false;
+
+    @Inject
+    public SplashPresenter(DaoSession daoSession) {
+        super(daoSession);
+    }
+
+    @Override
+    public void getUser() {
+        AuthUserDao authUserDao = daoSession.getAuthUserDao();
+
+        List<AuthUser> users = authUserDao.queryBuilder()
+                .where(AuthUserDao.Properties.Selected.eq(true))
+                .limit(1)
+                .list();
+
+        AuthUser selectedUser = users != null && users.size() > 0 ? users.get(0) : null;
+
+        //if none selected, choose first account
+        if(selectedUser == null){
+            List<AuthUser> firstAccount = authUserDao.queryBuilder()
+                    .limit(1)
+                    .list();
+            selectedUser = firstAccount != null && firstAccount.size() > 0 ? firstAccount.get(0) : null;
+        }
+
+        if (selectedUser != null && isExpired(selectedUser)) {
+            authUserDao.delete(selectedUser);
+            selectedUser = null;
+        }
+
+        if (selectedUser != null) {
+            AppData.INSTANCE.setAuthUser(selectedUser);
+            getUserInfo(selectedUser.getAccessToken());
+        } else {
+            mView.showLoginPage();
+        }
+
+    }
+
+    @Override
+    public void saveAccessToken(String accessToken, String scope, int expireIn) {
+        AuthUser authUser = new AuthUser();
+        authUser.setSelected(true);
+        authUser.setScope(scope);
+        authUser.setExpireIn(expireIn);
+        authUser.setAuthTime(new Date());
+        authUser.setAccessToken(accessToken);
+        daoSession.getAuthUserDao().insert(authUser);
+        this.authUser = authUser;
+    }
+
+    private void getUserInfo(final String accessToken) {
+
+        HttpObserver<User> httpObserver = new HttpObserver<User>() {
+            @Override
+            public void onError(@NonNull Throwable error) {
+                daoSession.getAuthUserDao().delete(AppData.INSTANCE.getAuthUser());
+                AppData.INSTANCE.setAuthUser(null);
+                mView.showErrorToast(getErrorTip(error));
+                mView.showLoginPage();
+            }
+
+            @Override
+            public void onSuccess(@NonNull HttpResponse<User> response) {
+                AppData.INSTANCE.setLoggedUser(response.body());
+                if (authUser != null) {
+                    authUser.setLoginId(response.body().getLogin());
+                    daoSession.getAuthUserDao().update(authUser);
+                }
+                if(!isMainPageShowwed) {
+                    isMainPageShowwed = true;
+                    mView.showMainPage();
+                }
+            }
+        };
+
+        generalRxHttpExecute(new IObservableCreator<User>() {
+            @Override
+            public Observable<Response<User>> createObservable(boolean forceNetWork) {
+                return getUserService().getPersonInfo(forceNetWork);
+            }
+        }, httpObserver, true);
+
+    }
+
+    private boolean isExpired(AuthUser selectedUser){
+        return selectedUser.getAuthTime().getTime() + selectedUser.getExpireIn() * 1000
+                < System.currentTimeMillis();
+    }
+
+}
