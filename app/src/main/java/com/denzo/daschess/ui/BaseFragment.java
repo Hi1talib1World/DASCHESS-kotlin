1e4075593d2a64a4bd42a5935b15f61031bcc75e
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-14 07:12:53 +0100

    Update BaseFragment.java

--- app/src/main/java/com/denzo/daschess/ui/BaseFragment.java
+++ app/src/main/java/com/denzo/daschess/ui/BaseFragment.java
@@ -6,14 +6,18 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.denzo.daschess.R;
 import com.google.android.material.snackbar.Snackbar;
 import com.thirtydegreesray.dataautoaccess.DataAutoAccess;
