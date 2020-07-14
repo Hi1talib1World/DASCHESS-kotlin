b3c1d0af6621b487df235aa3fd9bc7e9dba38fef
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-14 06:58:48 +0100

    Update BaseFragment.java

--- app/src/main/java/com/denzo/daschess/ui/BaseFragment.java
+++ app/src/main/java/com/denzo/daschess/ui/BaseFragment.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
