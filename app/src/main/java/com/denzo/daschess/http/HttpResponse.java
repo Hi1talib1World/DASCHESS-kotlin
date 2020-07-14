da53d06e42f1614cc41db49a41c73407030cb7cd
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-14 10:30:06 +0100

    Create HttpResponse.java

---
+++ app/src/main/java/com/denzo/daschess/http/HttpResponse.java
@@ -0,0 +1,44 @@
+package com.denzo.daschess.http;
+
+
+import androidx.annotation.Nullable;
+
+import retrofit2.Response;
+
+/**
+ * Created on 2017/7/13.
+ *
+ * @author ThirtyDegreesRay
+ */
+
+public class HttpResponse <T extends Object> {
+
+    private Response<T> oriResponse;
+
+    public HttpResponse(Response<T> response){
+        oriResponse = response;
+    }
+
+    public boolean isSuccessful(){
+        return oriResponse.isSuccessful();
+    }
+
+    public boolean isFromCache(){
+        return  isResponseEnable(oriResponse.raw().cacheResponse());
+    }
+
+    public boolean isFromNetWork(){
+        return  isResponseEnable(oriResponse.raw().networkResponse());
+    }
+
+    private boolean isResponseEnable(@Nullable okhttp3.Response response){
+        return response != null && response.code() == 200;
+    }
+
+    public Response<T> getOriResponse() {
+        return oriResponse;
+    }
+
+    public T body(){
+        return oriResponse.body();
+    }}
