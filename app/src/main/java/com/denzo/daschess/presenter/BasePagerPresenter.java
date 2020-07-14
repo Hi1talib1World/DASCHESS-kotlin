09397961da4242830eb9ba6cfe65a5366fe991a4
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-14 07:26:24 +0100

    Create BasePagerPresenter.java

---
+++ app/src/main/java/com/denzo/daschess/presenter/BasePagerPresenter.java
@@ -0,0 +1,40 @@
+package com.denzo.daschess.presenter;
+
+
+import com.denzo.daschess.ui.IBasePagerContract;
+
+public abstract class BasePagerPresenter<V extends IBasePagerContract.View> extends BasePresenter<V>
+        implements IBasePagerContract.Presenter<V>{
+
+    private boolean isLoaded = false;
+
+    public BasePagerPresenter(DaoSession daoSession) {
+        super(daoSession);
+    }
+
+    @Override
+    public void onViewInitialized() {
+        super.onViewInitialized();
+        prepareLoadData();
+    }
+
+    @Override
+    public void prepareLoadData() {
+        if(mView == null) {
+            return;
+        }
+        if(mView.isPagerFragment() && (!isViewInitialized() || !mView.isFragmentShowed())){
+            return;
+        }
+        if(isLoaded) return;
+        isLoaded = true;
+        loadData();
+    }
+
+    public void setLoaded(boolean loaded) {
+        isLoaded = loaded;
+    }
+
+    protected abstract void loadData();
+
+}
