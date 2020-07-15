8cd678101ca4638adf26f70fb233be20454d16cc
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-15 16:25:20 +0100

    Update LocalUserDao.java

--- app/src/main/java/com/denzo/daschess/dao/LocalUserDao.java
+++ app/src/main/java/com/denzo/daschess/dao/LocalUserDao.java
@@ -1,6 +1,13 @@
 package com.denzo.daschess.dao;
 
 import android.database.Cursor;
+import android.database.sqlite.SQLiteStatement;
+
+import org.greenrobot.greendao.AbstractDao;
+import org.greenrobot.greendao.Property;
+import org.greenrobot.greendao.database.Database;
+import org.greenrobot.greendao.database.DatabaseStatement;
+import org.greenrobot.greendao.internal.DaoConfig;
 
 public class LocalUserDao extends AbstractDao<LocalUser, String> {
 
