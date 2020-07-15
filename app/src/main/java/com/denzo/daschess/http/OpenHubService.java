5d2469d024984a9e05ee5d9e06a0f808c6736dea
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-15 02:00:01 +0100

    Update OpenHubService.java

--- app/src/main/java/com/denzo/daschess/http/OpenHubService.java
+++ app/src/main/java/com/denzo/daschess/http/OpenHubService.java
@@ -1,4 +1,32 @@
 package com.denzo.daschess.http;
 
-class OpenHubService {
+
+import androidx.annotation.NonNull;
+
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Query;
+
+public interface OpenHubService {
+
+    /**
+     * get trending repos, update every day
+     * @param since daily, weekly, monthly
+     * @param language language
+     */
+    @Headers("Cache-Control: public, max-age=86400")
+    @NonNull
+    @GET("trending")
+    Observable<Response<ArrayList<Repository>>> getTrendingRepos(
+            @Query("since") String since,
+            @Query("language") String language
+    );
+
+    /**
+     * get trending languages, update every day
+     */
+    @Headers("Cache-Control: public, max-age=86400")
+    @NonNull @GET("languages")
+    Observable<Response<ArrayList<TrendingLanguage>>> getTrendingLanguages();
+
 }
