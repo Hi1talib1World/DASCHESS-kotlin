8cc22c7ab43c73d9b1781a4bf4979e726911d1a8
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-07-04 11:55:58 +0100

    chore: update AndroidManifest configuration

---
+++ app/src/main/java/com/denzo/daschess/RegisterFragment.kt
@@ -0,0 +1,25 @@
+package com.denzo.daschess
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class RegisterFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btn_register).setOnClickListener {
+            val intent = Intent(requireContext(), MainActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+    }
+}
